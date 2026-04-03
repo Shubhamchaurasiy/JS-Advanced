a51be46b4509e39c4953a113ee4b80376425f389
Shubhamchaurasiy <91664093+Shubhamchaurasiy@users.noreply.github.com>
2026-04-03 12:53:20 +0000

    datatype conversion

---
+++ Basic_01/DataType.js
@@ -0,0 +1,32 @@
+"use strict";//trate all js code as newer version
+//alert("hello shubham");
+//we can't user alrt in this formate in node js
+//code readablity should be high 
+//ecma deefine the standerd of js==>tc39.es
+let name="shubham";//2 to power 53
+let age=19;
+let isLoggedIn=false;
+let state;
+//number===>2 to power 53
+//bigint===>use in stock i data
+///boolen ==>true false
+//null===>standalone value>>>special type for empty value//tempreture null mean tempreature not  come;
+//symbol===to find uniqnes
+//undefine===>value not define 
+///object
+//console.log(typeof undefined);
+
+//NAN==not a number 
+
+//conversion and operation
+
+
+let score=null;
+console.log(typeof score);//string
+let valurInNumber=Number(score);
+console.log( valurInNumber);
+console.log(typeof valurInNumber);//number
+///"33"==>33
+//"33ss">>>NAn
+//TRUE>>>1;FALSE>>0
+ ///1>>TRUE;0>>>FALSE;"">>>FALSE;"HSDHJDS">TRUE
