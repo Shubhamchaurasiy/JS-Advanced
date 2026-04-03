25289212c794ef333ee9bb6bf20988539e36c833
Shubhamchaurasiy <91664093+Shubhamchaurasiy@users.noreply.github.com>
2026-04-03 09:50:34 +0000

    DATAtYPE

---
+++ Basic_01/Variable.js
@@ -0,0 +1,18 @@
+const accountId = 1232;//never changed;
+let accountEmail="shubham@gmail.com";
+var accountPassword ="shubham@123";
+accountCity="jaipur";
+accountCity="rewa";
+let accountState;
+//accountId=2;
+accountEmail="shivam@gmmail.com";
+accountPassword="shubhdfdcsdam@123";
+//console.log(accountId);
+//console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+console.table([accountEmail,accountPassword,accountId,accountPassword,accountState]);
+if(8>4){
+     accountCity="shhs";
+    console.log(accountCity);
+}
